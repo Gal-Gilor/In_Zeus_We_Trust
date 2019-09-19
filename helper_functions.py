589c7e9750b2e360db3dc4f20c1fceaf207923f9
Gal Gilor <gilorgal2010@gmail.com>
2019-09-19 15:14:07 -0400

    changed module

--- helper_functions.py
+++ helper_functions.py
@@ -1,23 +1,23 @@
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import recall_score, accuracy_score, roc_curve, auc, confusion_matrix
+from bs4 import BeautifulSoup
+import requests
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+
 plt.style.use('ggplot')
-import requests
-from bs4 import BeautifulSoup
 
-from sklearn.metrics import recall_score, accuracy_score, roc_curve, auc, confusion_matrix
-from sklearn.model_selection import train_test_split, GridSearchCV
 
-### FUNCTIONS USED IN THE EDA PROCCES
+# FUNCTIONS USED IN THE EDA PROCCES
+
 
 def populate_df(df):
     """ This function turns the characters dataframe into dummy varaibles dataframe"""
     uniques = pd.unique(df.values.ravel('K'))
     zeros = np.zeros(len(uniques))
-    
-    
+
     all_dummies = []
     for row in df.itertuples():
         i = 1
@@ -25,60 +25,63 @@ def populate_df(df):
         while i < 6:
             uniques_dic[row[i]] = 1
             i += 1
-            
-        all_dummies.append(uniques_dic)       
+
+        all_dummies.append(uniques_dic)
     return pd.DataFrame(all_dummies, columns=uniques)
+
 
 def character_by_attributes(attribute):
     '''This function scrapes all of the heroes by their main attribute
        off of dota2 wikisite'''
     # Make a get request to retrieve the page
-    html = requests.get(f'https://dota2.gamepedia.com/{attribute}') 
+    html = requests.get(f'https://dota2.gamepedia.com/{attribute}')
 
     # Pass the page contents to beautiful soup for parsing
     soup = BeautifulSoup(html.content, 'html.parser')
 
     # search for the characters by attribute
-    char_raw = soup.findAll('td',{'style':"white-space:nowrap;"})
-    char_list = np.array([item.find('a').attrs['href'].replace('/', '') for item in char_raw])
+    char_raw = soup.findAll('td', {'style': "white-space:nowrap;"})
+    char_list = np.array(
+        [item.find('a').attrs['href'].replace('/', '') for item in char_raw])
     return char_list
 
 
 def create_hist(df, column, save=None):
     ''' This function creates an histogram using a dataframe and a column name '''
-    plt.figure(figsize=(8, 5))  
+    plt.figure(figsize=(8, 5))
 
-    # Remove the plot frame lines. 
-    ax = plt.subplot(111)  
-    ax.spines["top"].set_visible(False)  
-    ax.spines["right"].set_visible(False)  
+    # Remove the plot frame lines.
+    ax = plt.subplot(111)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
     plt.grid(b=None)
-    
+
     # set labels
     plt.title(f"{column} Heroes in Team Histogram")
-    plt.xlabel(f"{column} Per Team", fontsize=16)  
-    plt.ylabel("Matches Won", fontsize=16)  
+    plt.xlabel(f"{column} Per Team", fontsize=16)
+    plt.ylabel("Matches Won", fontsize=16)
 
-    # Plot the histogram  
+    # Plot the histogram
     plt.hist(df[column], bins=5, alpha=0.7, density=True)
     plt.show()
     if save:
         plt.savefig(f'{column}_Histogram.png')
     pass
 
-### FUNCTIONS USED IN THE MODELING PROCCES
+# FUNCTIONS USED IN THE MODELING PROCCES
+
 
 def print_metrics(labels, predictions, print_score=None):
     ''' This function receives model predictions along with the actual labels
         and returns the precision score, recall, accuracy and F1'''
-    
+
     recall = round(recall_score(labels, predictions)*100, 2)
     acc = round(accuracy_score(labels, predictions)*100, 2)
-    
+
     if print_score:
         print(f"Recall Score: {recall}")
         print(f"Accuracy Score: {acc}")
-        
+
     return recall, acc
 
 
@@ -86,27 +89,27 @@ def plot_confusion_matrix(y_test, y_pred):
     matrix = confusion_matrix(y_test, y_pred)
     plt.matshow(matrix,  cmap=plt.cm.RdYlBu, aspect=1.75, alpha=0.5)
 
-    #Add title and Axis Labels
+    # Add title and Axis Labels
     plt.ylabel('Actual')
     plt.xlabel('Predicted')
 
-    #Add appropriate Axis Scales
+    # Add appropriate Axis Scales
     class_names = ['Lose', 'Win']
     tick_marks = np.arange(len(class_names))
     plt.xticks(tick_marks, class_names, rotation=45)
     plt.yticks(tick_marks, class_names)
     plt.grid(b=None)
-    
-    #Add Labels to Each Cell
-    thresh = matrix.max() / 2. #Used for text coloring below
-    
-    #iterate through the confusion matrix and append the labels
-    for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
-            plt.text(j, i, matrix[i, j],
-                     horizontalalignment="center",
-                     color="black" if matrix[i, j] > thresh else "black")
 
-    #Add a Side Bar Legend Showing Colors
+    # Add Labels to Each Cell
+    thresh = matrix.max() / 2.  # Used for text coloring below
+
+    # iterate through the confusion matrix and append the labels
+    for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
+        plt.text(j, i, matrix[i, j],
+                 horizontalalignment="center",
+                 color="black" if matrix[i, j] > thresh else "black")
+
+    # Add a Side Bar Legend Showing Colors
     plt.colorbar()
     return
 
@@ -115,34 +118,35 @@ def plot_feature_importance(model, x_train, n=30):
     """ This function recievies a model and plots the 'n' most important features"""
     # extract and sort the feature importance
     features = model.feature_importances_
-    columns = x_train.columns.values
-    
+    feature_names = x_train.columns.values
+
     # combine the features importance and column names into a matrix and sort them
-    feature_matrix = np.array([all_feat, feature_names])
+    feature_matrix = np.array([features, feature_names])
     feature_matrix = feature_matrix.transpose()
     feature_matrix.sort(0)
-    
+
     # divide the column names and feature importance
     sorted_feat = feature_matrix[:, 0]
     sorted_columns = feature_matrix[:, 1]
-    
-     # plot the features
+
+    # plot the features
     plt.figure(figsize=(16, 12))
     try:
         plt.barh(sorted_columns[-n:], sorted_feat[-n:], align='center')
-    
+
     except:
         # if n features is greater than the amount that actually exists
         n = len(sorted_feat)
         plt.barh(sorted_columns[-n:], sorted_feat[-n:], align='center')
-        
+
     plt.yticks(sorted_columns[-n:], sorted_columns[-n:])
     plt.xlabel("Feature Importance")
     plt.ylabel("Feature")
     return
 
-def find_optimal_depth(x_train, x_test, y_train, y_test):   
-    #declare variables
+
+def find_optimal_depth(x_train, x_test, y_train, y_test):
+    # declare variables
     max_depths = np.linspace(1, 20, 20, endpoint=True)
     train_results = []
     test_results = []
@@ -150,20 +154,20 @@ def find_optimal_depth(x_train, x_test, y_train, y_test):
     for depth in max_depths:
         trees = DecisionTreeClassifier(criterion='entropy', max_depth=depth)
         trees.fit(x_train, y_train)
-        
+
         # Add auc score to train list
         train_pred = trees.predict(x_train)
         fpr, tpr, thresholds = roc_curve(y_train, train_pred)
         roc_auc = auc(fpr, tpr)
         train_results.append(roc_auc)
-        
+
         # Add auc score to test list
         test_pred = trees.predict(x_test)
         fpr, tpr, thresholds = roc_curve(y_test, test_pred)
         roc_auc = auc(fpr, tpr)
         test_results.append(roc_auc)
-        
-    plt.figure(figsize=(10,7))
+
+    plt.figure(figsize=(10, 7))
     plt.plot(max_depths, train_results, 'b', label='Train AUC')
     plt.plot(max_depths, test_results, 'r', label='Test AUC')
     plt.ylabel('AUC score')
